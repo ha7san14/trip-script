5e98176226b03ba2ec52a59969216dc5b60d54d0
DESKTOP-SFRIP5L\Z B O O K <hassanikram1065@gmail.com>
2025-07-19 01:39:14 +0500

    Project Setup

---
+++ tripscript/src/main/java/com/tripscript/app/TripScriptApplication.java
@@ -0,0 +1,13 @@
+package com.tripscript.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripScriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripScriptApplication.class, args);
+	}
+
+}
---
+++ tripscript/src/test/java/com/tripscript/app/TripScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripscript.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripScriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
